1be54bc67ec62e52e349c497ca23ca6ecc5de2d4
aakashkbw <114473332+aakashkbw@users.noreply.github.com>
2023-05-07 01:10:37 +0200

    Durchschnittseite Verbesserungen

--- versicherungs_applikation/src/App.jsx
+++ versicherungs_applikation/src/App.jsx
@@ -8,14 +8,6 @@ import Durchschnitt from "./Pages/Durchschnitt";
 function App() {
   return (
     <>
-    <nav>
-      <ul className="navigation_ul">
-        <li><Link to="/"> Home </Link></li>
-        <li><Link to="/erste"> Auswahl </Link></li>
-        <li><Link to="/summe"> Summe </Link></li>
-        <li><Link to="/durchschnitt"> Schnitt </Link></li>
-      </ul>
-    </nav>
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/erste" element={<Erste />} />
--- versicherungs_applikation/src/Pages/Durchschnitt.js
+++ versicherungs_applikation/src/Pages/Durchschnitt.js
@@ -1,4 +1,12 @@
 import React from "react";
+import "../Durchschnitt.css";
+import cat from "../Images/cat.jpg";
+import tiger from "../Images/tiger.jpg";
+import dear from "../Images/dear.jpg";
+import giraffe from "../Images/giraffe.jpg";
+import moose from "../Images/moose.jpg";
+import lion from "../Images/lion.jpg";
+import dog from "../Images/dog.jpg";
 
 function Durchschnitt() {
   return (
@@ -7,6 +15,21 @@ function Durchschnitt() {
         Hoppala! Das geht wohl leider nicht. Versuchen Sie es mit der eigenen
         Summe!
       </h2>
+
+      <h3>
+        Gerne können Sie, falls Sie zufälligerweise zu viel Zeit haben, diese
+        unnötige Gallerie durchschauen!
+      </h3>
+
+      <div className="gallery">
+        <img src={cat} alt="cat" />
+        <img src={tiger} alt="tiger" />
+        <img src={dear} alt="dear" />
+        <img src={giraffe} alt="giraffe" />
+        <img src={moose} alt="moose" />
+        <img src={lion} alt="lion" />
+        <img src={dog} alt="dog" />
+      </div>
     </div>
   );
 }
