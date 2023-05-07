8954ef51f2f965ad4e246ee4faca2db229cd839a
aakashkbw <114473332+aakashkbw@users.noreply.github.com>
2023-05-07 13:50:38 +0200

    progressbar

--- versicherungs_applikation/src/App.jsx
+++ versicherungs_applikation/src/App.jsx
@@ -1,4 +1,5 @@
-import { Link, Route, Routes } from "react-router-dom";
+import { useState, useEffect } from "react";
+import { Link, Route, Routes, useLocation } from "react-router-dom";
 import "./App.css";
 import { Home } from "./Pages/Home";
 import Erste from "./Pages/Erste";
@@ -6,14 +7,36 @@ import Summe from "./Pages/Summe";
 import Durchschnitt from "./Pages/Durchschnitt";
 
 function App() {
+  const location = useLocation();
+  const [progress, setProgress] = useState(0);
+
+  useEffect(() => {
+    let progressPercentage = 0;
+    switch (location.pathname) {
+      case "/erste":
+        progressPercentage = 33;
+        break;
+      case "/summe":
+        progressPercentage = 100;
+        break;
+      case "/durchschnitt":
+        progressPercentage = 66;
+        break;
+      default:
+        progressPercentage = 0;
+    }
+    setProgress(progressPercentage);
+  }, [location]);
+
   return (
     <>
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/erste" element={<Erste />} />
-      <Route path="/summe" element={<Summe />} />
-      <Route path="/durchschnitt" element={<Durchschnitt />} />
-    </Routes>
+      <div className="progress-bar" style={{ width: `${progress}%` }}></div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/erste" element={<Erste />} />
+        <Route path="/summe" element={<Summe />} />
+        <Route path="/durchschnitt" element={<Durchschnitt />} />
+      </Routes>
     </>
   );
 }
